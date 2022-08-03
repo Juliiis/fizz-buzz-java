6b524af5f1a2442462ff059e29aed5cec3271c27
Julieta Martin <julieta.martin@clarity.ai>
2022-08-03 20:41:14 +0200

    logic solution solved

---
+++ app/src/main/java/gradle/java/FizzBuzz.java
@@ -0,0 +1,15 @@
+package gradle.java;
+
+public class FizzBuzz {
+    public String fizzBuzz(int number){
+
+        if(number % 15 == 0){
+            return "FizzBuzz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        }
+        return String.valueOf(number);
+    }
+}
